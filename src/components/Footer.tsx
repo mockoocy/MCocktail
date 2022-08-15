4167dd185f431f28088973d15b90b43da0845569
Ocycak <pawlikm20q@gmail.com>
2022-08-15 15:23:31 +0200

    added github link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import StyledFooter from "./styles/StyledFooter"
+import StyledFooter from "./styles/StyledFooter";
+import { Icon } from "@iconify/react";
 
 export default function Footer(){
   return(
     <StyledFooter>
       <h4>Hope you enjoy being here!</h4>
-      <h2>Made by Mockini @2022</h2>
+      <a className="credits" href="https://github.com/Ocycak">
+      <Icon icon="line-md:github" className="svg"/><h2>Made by Mockini @2022</h2>
+      </a>
       <h4>M stands for Make, Mix and Mocktail</h4>
     </StyledFooter>
   )
--- src/components/styles/StyledFooter.ts
+++ src/components/styles/StyledFooter.ts
@@ -10,6 +10,16 @@ width: 100%;
 position: relative;
 bottom: 0;
 
+.credits, .credits:visited, .credits:link {
+  color: var(--fancyClr);
+  display: flex;
+  align-items: center;
+  .svg {
+    color: var(--fancyClr);
+    width: 2rem;
+    height:2rem;
+  }
+}
 @media (max-width:820px) {
   font-size: 0.75rem;
 }
