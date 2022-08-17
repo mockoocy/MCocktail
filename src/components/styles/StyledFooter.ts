cdecd733fcfbbbfd575c50e46ea64e5ecf6874f1
Ocycak <pawlikm20q@gmail.com>
2022-08-17 14:21:55 +0200

    last commit for a while probably

--- src/components/styles/StyledFooter.ts
+++ src/components/styles/StyledFooter.ts
@@ -9,11 +9,25 @@ background-color: var(--captionClr);
 width: 100%;
 position: relative;
 bottom: 0;
+font-family: Lato, Inter, sans-serif;
 
+h4 {
+  font-size: 1.5rem;
+  @media (max-width: 820px){
+    font-size: .75rem
+  }
+}
 .credits, .credits:visited, .credits:link {
   color: var(--fancyClr);
   display: flex;
   align-items: center;
+  font-size: 2rem;
+
+  @media (max-width: 820px){
+    font-size: 1rem;
+  }
+  
+  
   .svg {
     color: var(--fancyClr);
     width: 2rem;
